399fb7954ab82786ca40acd29edbfcc865836fbd
Vlad Khytrov <vkhytrov@engagepeople.com>
2020-06-04 15:49:13 +0200

    Implemented contact pick screen.

--- app/src/main/java/com/aegis/safetyalarm/PickContactActivity.kt
+++ app/src/main/java/com/aegis/safetyalarm/PickContactActivity.kt
@@ -1,12 +1,65 @@
 package com.aegis.safetyalarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_pick_contact.*
 
 class PickContactActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pick_contact)
+
+        btn_back_pc.setOnClickListener {
+            onBackPressed()
+        }
+
+        btn_choose_phone_contact.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE
+            startActivityForResult(intent, 1)
+        }
+
+        btn_confirm_contact.setOnClickListener {
+            val number = et_number_pick.text.toString().trim()
+            if (number.isEmpty()) {
+                Toast.makeText(this, "Phone number is empty", Toast.LENGTH_SHORT).show()
+            } else {
+                val name = et_name_pick.text.toString().trim()
+                val data = Intent().apply {
+                    putExtra("number", number)
+                    putExtra("name", name)
+                }
+                setResult(Activity.RESULT_OK, data)
+                finish()
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                val uri = data.data
+                val cursor = contentResolver.query(uri!!, null, null, null, null)
+                cursor!!.moveToFirst()
+                val phoneIndex =
+                    cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                val nameIndex =
+                    cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+                val phoneNo = cursor.getString(phoneIndex)
+                val name = cursor.getString(nameIndex)
+
+                et_name_pick.setText(name)
+                et_number_pick.setText(phoneNo)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/aegis/safetyalarm/SettingsActivity.kt
+++ app/src/main/java/com/aegis/safetyalarm/SettingsActivity.kt
@@ -1,14 +1,14 @@
 package com.aegis.safetyalarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import com.aegis.safetyalarm.R
+import androidx.appcompat.app.AppCompatActivity
 import com.aegis.safetyalarm.data.ContactStorage
 import com.aegis.safetyalarm.data.UserStorage
 import kotlinx.android.synthetic.main.activity_settings.*
 
 // todo validation
-// todo phone contacts provider
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,15 +23,35 @@ class SettingsActivity : AppCompatActivity() {
         val contactStorage = ContactStorage(this)
 
         val name = userStorage.getName()
-        val contact = contactStorage.getContact()
+        val number = contactStorage.getContact()
         et_name.setText(name)
-        et_contact.setText(contact)
+        et_contact.setText(number)
 
         btn_save.setOnClickListener {
             val newName = et_name.text.toString().trim()
             val newContact = et_contact.text.toString().trim()
             userStorage.setName(newName)
             contactStorage.setContact(newContact)
+        }
+
+        btn_add_contact.setOnClickListener {
+            val intent = Intent(this, PickContactActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            val number = data.getStringExtra("number")
+            val name = data.getStringExtra("name")
+            et_name.setText(name)
+            et_contact.setText(number)
+
+            val userStorage = UserStorage(this)
+            val contactStorage = ContactStorage(this)
+            userStorage.setName(name)
+            contactStorage.setContact(number)
         }
     }
 
