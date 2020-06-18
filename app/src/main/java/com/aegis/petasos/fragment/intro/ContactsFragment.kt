eaebe2cf916b69d3ab24fe96ef39616fffee49bc
Vlad Khytrov <vkhytrov@engagepeople.com>
2020-06-18 17:44:51 +0200

    Onboarding almost completed.

--- app/src/main/java/com/aegis/petasos/fragment/intro/ContactsFragment.kt
+++ app/src/main/java/com/aegis/petasos/fragment/intro/ContactsFragment.kt
@@ -1,13 +1,122 @@
 package com.aegis.petasos.fragment.intro
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.aegis.petasos.App
 import com.aegis.petasos.R
+import com.aegis.petasos.activity.PickContactActivity
+import com.aegis.petasos.data.db.Contact
+import com.aegis.petasos.viewmodel.ContactsViewModel
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.fragment_intro_contacts.*
 
 class ContactsFragment : Fragment(R.layout.fragment_intro_contacts) {
 
+    private val RQ_CODE_EMERGENCY = 1
+    private val RQ_CODE_SECONDARY = 2
+
+    private lateinit var contactsViewModel: ContactsViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        contactsViewModel = ViewModelProvider(
+            this,
+            ContactsViewModel.Factory(
+                (requireActivity().application as App).db
+            )
+        ).get(ContactsViewModel::class.java)
+
+        contactsViewModel.emergencyContacts.observe(this, Observer {
+            renderEmContacts(it)
+        })
+
+        contactsViewModel.secondaryContacts.observe(this, Observer {
+            renderSecContacts(it)
+        })
+
     }
+
+    private fun renderEmContacts(contacts: List<Contact>) {
+        chip_group_em_intro.removeAllViews()
+        contacts.forEach {
+            chip_group_em_intro.addView(getChipForContact(it))
+        }
+        val addChip = createAddContactChip()
+        addChip.setOnClickListener { v ->
+            val intent = Intent(requireContext(), PickContactActivity::class.java)
+            startActivityForResult(intent, RQ_CODE_EMERGENCY)
+        }
+        chip_group_em_intro.addView(addChip)
+    }
+
+    private fun renderSecContacts(contacts: List<Contact>) {
+        chip_group_sec_intro.removeAllViews()
+        contacts.forEach {
+            chip_group_sec_intro.addView(getChipForContact(it))
+        }
+        val addChip = createAddContactChip()
+        addChip.setOnClickListener { v ->
+            val intent = Intent(requireContext(), PickContactActivity::class.java)
+            startActivityForResult(intent, RQ_CODE_SECONDARY)
+        }
+        chip_group_sec_intro.addView(addChip)
+    }
+
+    private fun createAddContactChip(): Chip {
+        val chip = Chip(requireContext())
+        chip.text = getString(R.string.add_contact)
+        chip.chipBackgroundColor = ContextCompat.getColorStateList(
+            requireContext(),
+            R.color.chip_color_selector
+        )
+        chip.chipIcon = ContextCompat.getDrawable(
+            requireContext(),
+            R.drawable.ic_add
+        )
+        return chip
+    }
+
+    private fun getChipForContact(c: Contact): Chip {
+        val chip = Chip(requireContext())
+        chip.text = if (c.name.isEmpty()) {
+            c.number
+        } else {
+            c.name
+        }
+        chip.isCloseIconVisible = true
+        chip.tag = c.id
+        chip.setOnCloseIconClickListener { v ->
+            contactsViewModel.deleteContactById(v!!.tag as Int)
+        }
+        return chip
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            val number = data.getStringExtra("number")
+            val name = data.getStringExtra("name")
+            val isEmergency =
+                if (requestCode == RQ_CODE_EMERGENCY) {
+                    1
+                } else {
+                    0
+                }
+            val newContact = Contact(
+                null,
+                name = name,
+                number = number,
+                isEmergencyContact = isEmergency
+            )
+            contactsViewModel.addContact(newContact)
+        }
+    }
+
 }
--- app/src/main/java/com/aegis/petasos/fragment/intro/NameFragment.kt
+++ app/src/main/java/com/aegis/petasos/fragment/intro/NameFragment.kt
@@ -2,12 +2,31 @@ package com.aegis.petasos.fragment.intro
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import com.aegis.petasos.R
+import com.aegis.petasos.data.UserStorage
+import kotlinx.android.synthetic.main.fragment_intro_name.*
 
 class NameFragment : Fragment(R.layout.fragment_intro_name) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        et_username_intro.doAfterTextChanged {
+            username_layout.error = null
+        }
     }
+
+    fun validateName(): Boolean {
+        val username = et_username_intro.text.toString().trim()
+        if (username.isEmpty()) {
+            username_layout.error = getString(R.string.validation_name)
+            return false
+        } else {
+            UserStorage(requireContext()).setName(username)
+            return true
+        }
+    }
+
 }
